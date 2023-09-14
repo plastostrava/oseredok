c10a2a9c5456c12d0bdac4736b6339617c6e6e10
plastostrava <145064354+plastostrava@users.noreply.github.com>
2023-09-14 22:04:52 +0200

    login.php

---
+++ login.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Login Page</title>
+</head>
+<body>
+    <h2>Login</h2>
+    <form action="login_process.php" method="POST">
+        <input type="text" name="username" placeholder="Username" required><br>
+        <input type="password" name="password" placeholder="Password" required><br>
+        <input type="submit" value="Login">
+    </form>
+</body>
+</html>
