51367a67e8e7dfba09865891843abf039e3b2310
plastostrava <145064354+plastostrava@users.noreply.github.com>
2023-09-14 22:03:01 +0200

    register.php

---
+++ register.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Registration Page</title>
+</head>
+<body>
+    <h2>Register</h2>
+    <form action="register_process.php" method="POST">
+        <input type="text" name="username" placeholder="Username" required><br>
+        <input type="email" name="email" placeholder="Email" required><br>
+        <input type="password" name="password" placeholder="Password" required><br>
+        <input type="submit" value="Register">
+    </form>
+</body>
+</html>
